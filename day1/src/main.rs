05c34237432dc0c3c3c98e86832205d68dceaec0
Ransom <alex@ransomti.me>
2025-12-01 22:58:40 +0000

    part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -30,9 +30,6 @@ fn clock_add(input: i32, add: i32, max: i32) -> i32 {
   sum
 }
 
-fn clock_sub(input: i32, sub: i32, max: i32) -> i32 {
-  clock_add(input, -sub, max)
-}
 
 
 fn part_1(input: String) -> i32 {
@@ -41,9 +38,9 @@ fn part_1(input: String) -> i32 {
   for line in input.lines() {
     let mut chars = line.chars();
     cur = match chars.next().unwrap() {
-      'L' => clock_sub(cur, chars.as_str().to_string().parse::<i32>().unwrap(), 99),
+      'L' => clock_add(cur, -chars.as_str().to_string().parse::<i32>().unwrap(), 99),
       'R' => clock_add(cur, chars.as_str().to_string().parse::<i32>().unwrap(), 99),
-       _   => unreachable!(),
+      _   => unreachable!(),
     };
     if cur == 0 {
       pw += 1;
@@ -53,7 +50,49 @@ fn part_1(input: String) -> i32 {
 }
 
 fn part_2(input: String) -> i32 {
-  part_1(input)
+  let mut cur = 50;
+  let mut pw = 0;
+  for line in input.lines() {
+    let mut chars = line.chars();
+    let clicks;
+    (cur, clicks) = match chars.next().unwrap() {
+      'L' => clock_sub_with_clicks(cur, chars.as_str().to_string().parse::<i32>().unwrap(), 99),
+      'R' => clock_add_with_clicks(cur, chars.as_str().to_string().parse::<i32>().unwrap(), 99),
+      _   => unreachable!(),
+    };
+    pw += clicks;
+  }
+  pw
+}
+
+fn clock_add_with_clicks(input: i32, add: i32, max: i32) -> (i32, i32) {
+  let mut sum = input + add;
+  let mut clicks = 0;
+  while sum > max {
+    sum -= max + 1;
+    clicks += 1;
+  }
+  while sum < 0 {
+    sum += max + 1;
+    clicks += 1;
+  }
+  (sum, clicks)
+}
+
+fn clock_sub_with_clicks(input: i32, sub: i32, max: i32) -> (i32, i32) {
+  let mut sum = input - sub;
+  let mut clicks = match input {
+    0 => -1,
+    _ => 0,
+  };
+  while sum < 0 {
+    sum += max + 1;
+    clicks += 1;
+  }
+  if sum == 0 {
+    clicks += 1;
+  }
+  (sum, clicks)
 }
 
 
@@ -65,27 +104,40 @@ mod tests {
   fn test_part_1() {
     assert_eq!(part_1(file_to_string("inputs/demo")), 3);
   }
-
-    #[test]
+  
+  #[test]
   fn test_clock_add() {
     assert_eq!(clock_add(99, 1, 99), 0);
     assert_eq!(clock_add(4, 1, 99), 5);
   }
-
+  
   #[test]
   fn test_clock_sub() {
     assert_eq!(clock_sub(0, 1, 99), 99);
     assert_eq!(clock_sub(4, 1, 99), 3);
   }
-
-  /*
-
+  
+  #[test]
+  fn test_clock_add_with_clicks() {
+    assert_eq!(clock_add_with_clicks(0, 100, 99), (0,1));
+    assert_eq!(clock_add_with_clicks(0, 200, 99), (0,2));
+  }
+  
+  
+  #[test]
+  fn test_part_2_starts_at_zero() {
+    assert_eq!(clock_sub_with_clicks(0,5,99),(95,0)); 
+  }
+  
+  #[test]
+  fn test_part_2_starts_and_ends_at_zero() {
+    assert_eq!(clock_sub_with_clicks(0, 400, 99), (0,4));
+  }
+  
   #[test]
   fn test_part_2() {
-    assert_eq!(part_2(file_to_string("inputs/demo")), 2286);
-  } */
-
-  #[test]
-  fn test_incorrect_answers() {
+    assert_eq!(part_2(file_to_string("inputs/demo")), 6);
+    assert_eq!(part_2(file_to_string("inputs/input")), 6671);
   }
+  
 }
