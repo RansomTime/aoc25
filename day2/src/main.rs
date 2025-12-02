2e8b7df136391b335bf9ddc45d8f8f710ea1019a
Ransom <alex@ransomti.me>
2025-12-02 23:58:51 +0000

    part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -23,7 +23,7 @@ fn main() {
 fn part_1(input: String) -> i128 {
   let mut res = 0;
   for line in input.split(',') {
-    res += split_range(line).filter(|id| is_only_repeating_digits(*id)).sum::<i128>();
+    res += split_range(line).filter(|id| is_only_two_repeating_digits(*id)).sum::<i128>();
   }
   res
 }
@@ -35,8 +35,8 @@ fn split_range(range: &str) -> std::ops::RangeInclusive<i128>  {
   start..=end
 }
 
-fn is_only_repeating_digits(input: i128) -> bool {
-  let input_str = format!("{input}");
+fn is_only_two_repeating_digits(input: i128) -> bool {
+  let input_str = input.to_string();
   if input_str.chars().count() % 2 == 1 {
     return false;
   }
@@ -45,43 +45,78 @@ fn is_only_repeating_digits(input: i128) -> bool {
 }
 
 fn part_2(input: String) -> i128 {
-  unimplemented!()
+  let mut res = 0;
+  for line in input.split(',') {
+    res += split_range(line).filter(|id| is_only_repeating_digits(*id)).sum::<i128>(); 
+  }
+  res
 }
 
-
+fn is_only_repeating_digits(input: i128) -> bool {
+  let input_str = input.to_string();
+  let mut input_str_chars = input_str.chars();
+  let half = input_str.chars().count() / 2;
+  let mut cnd = String::from("");
+  
+  for i in 0..half {
+    cnd.push(input_str_chars.next().unwrap()); //push the next str from input into it
+    if input_str.chars().count() % (i + 1) != 0 {
+      // optimisation: if cnd won't go cleanly into input, skip 
+      continue;
+    }
+    let mut cmp = String::from("");
+    while cmp.chars().count() < input_str.chars().count() {
+      cmp = cmp + &cnd;
+    }
+    if cmp == input_str {
+      return true;
+    }
+  }
+  false
+}
 
 #[cfg(test)]
 mod tests {
   use super::*;
-
+  
   #[test]
   fn test_part_1() {
-  assert_eq!(part_1(file_to_string("inputs/demo")), 1227775554);
-  assert_eq!(part_1(file_to_string("inputs/input")),15873079081);
+    assert_eq!(part_1(file_to_string("inputs/demo")), 1227775554);
+    assert_eq!(part_1(file_to_string("inputs/input")),15873079081);
   }
-
-    #[test]
+  
+  #[test]
   fn test_part_1_range() {
-  assert_eq!(split_range("11-22"), 11..=22);
+    assert_eq!(split_range("11-22"), 11..=22);
   }
-
+  
   #[test]
   fn test_part_1_repeating_string_odd() {
-    assert_eq!(is_only_repeating_digits(1), false);
-    assert_eq!(is_only_repeating_digits(11), true);
-    assert_eq!(is_only_repeating_digits(211), false);
+    assert_eq!(is_only_two_repeating_digits(1), false);
+    assert_eq!(is_only_two_repeating_digits(11), true);
+    assert_eq!(is_only_two_repeating_digits(211), false);
   }
-
+  
   #[test]
   fn test_part_1_repeating_string_two_digit() {
-    assert_eq!(is_only_repeating_digits(21), false);
-    assert_eq!(is_only_repeating_digits(99), true);
+    assert_eq!(is_only_two_repeating_digits(21), false);
+    assert_eq!(is_only_two_repeating_digits(99), true);
   }
-    /*
+  #[test]
+  fn test_part_2_repeating_string_three_times() {
+    assert_eq!(is_only_repeating_digits(111), true);
+  }
+  
+  #[test]
+  fn test_part_2_repeating_string_simple() {
+    assert_eq!(is_only_repeating_digits(12), false);
+  }
+  
   #[test]
   fn test_part_2() {
-  assert_eq!(part_2(file_to_string("inputs/demo")), 2286);
-  } */
+    assert_eq!(part_2(file_to_string("inputs/demo")), 4174379265);
+    assert_eq!(part_2(file_to_string("inputs/input")),22617871034);
+  } 
   
   #[test]
   fn test_incorrect_answers() {
